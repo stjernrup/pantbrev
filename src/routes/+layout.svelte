64dfdb724601742f6f1fa5230d0576f02a398f18
John Stjernrup <stjernrup@gmail.com>
2025-02-09 22:37:24 +0100

    speed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
 	import '../app.css';
 	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
 	injectAnalytics();
+	injectSpeedInsights();
+
 	let { children } = $props();
 </script>
 
